766bb04a5e453f094a10054fb90c59fe3464ad49
Al <alexseier12345@gmail..com>
2021-02-23 20:51:38 +0300

    done hm_2

--- lesson2/hm_2/script/script.js
+++ lesson2/hm_2/script/script.js
@@ -32,40 +32,114 @@
 
 let a = 0;
 let lowLimit = 0;
-let resul = a;
+let result = a;
 
 switch (a) {
     case lowLimit:
-        resul = `${resul} ${++a}`;
+        result = `${result} ${++a}`;
     case ++lowLimit:
-        resul = `${resul} ${++a}`;
+        result = `${result} ${++a}`;
     case ++lowLimit:
-        resul = `${resul} ${++a}`;
+        result = `${result} ${++a}`;
     case ++lowLimit:
-        resul = `${resul} ${++a}`;
+        result = `${result} ${++a}`;
     case ++lowLimit:
-        resul = `${resul} ${++a}`;
+        result = `${result} ${++a}`;
     case ++lowLimit:
-        resul = `${resul} ${++a}`;
+        result = `${result} ${++a}`;
     case ++lowLimit:
-        resul = `${resul} ${++a}`;
+        result = `${result} ${++a}`;
     case ++lowLimit:
-        resul = `${resul} ${++a}`;
+        result = `${result} ${++a}`;
     case ++lowLimit:
-        resul = `${resul} ${++a}`;
+        result = `${result} ${++a}`;
     case ++lowLimit:
-        resul = `${resul} ${++a}`;
+        result = `${result} ${++a}`;
     case ++lowLimit:
-        resul = `${resul} ${++a}`;
+        result = `${result} ${++a}`;
     case ++lowLimit:
-        resul = `${resul} ${++a}`;
+        result = `${result} ${++a}`;
     case ++lowLimit:
-        resul = `${resul} ${++a}`;
+        result = `${result} ${++a}`;
     case ++lowLimit:
-        resul = `${resul} ${++a}`;
+        result = `${result} ${++a}`;
     case ++lowLimit:
-        resul = `${resul} ${++a}`;
+        result = `${result} ${++a}`;
 }
 
-console.log(resul)
+console.log(result)
 
+// 5
+
+const sum = function sum(a, b) {
+    return a + b;
+};
+
+let check = sum(5, -2);
+console.log(check);
+
+const subtract = function subtract(a, b) {
+    return a - b;
+};
+
+check = subtract(5, -2);
+console.log(check);
+
+const multiplication = function multiplication(a, b) {
+    return a * b;
+};
+
+check = multiplication(5, -2);
+console.log(check);
+
+const division = function division(a, b) {
+    return a / b;
+};
+
+check = division(5, -2);
+console.log(check);
+
+// 6
+
+const mathOperation = function mathOperation(arg1, arg2, operation) {
+
+    if (typeof arg1 !== 'number' || typeof arg2 !== 'number') return "Неправильно введеные данные"
+
+    switch (operation) {
+        case '+':
+            return sum(arg1, arg2);
+        case '-':
+            return subtract(arg1, arg2);
+        case '*':
+            return multiplication(arg1, arg2);
+        case '/':
+            return division(arg1, arg2);
+        default:
+            return "Неправильно введеные данные";
+    }
+}
+
+check = mathOperation(-2, 19, '*');
+console.log(check)
+
+// 7
+
+console.log(null > 0)
+console.log(null >= 0)  // нестрогое равенство и сравнения > < >= <= работают по-разному, сравнения преобразуют null в число, рассматривая его как 0.
+console.log(null <= 0)
+console.log(null == 0)
+
+// 8*
+
+const power = function power(val, pow) {
+
+    if (pow === 0) return 1;
+
+    if (pow === 1) return val
+    return power(val, (pow - 1)) * val;
+
+
+}
+
+check = power(2, 5);
+console.log(check)
