a38c439f4b2a8535b50f6c899f8b600f12b08343
Al <alexseier12345@gmail..com>
2021-03-14 13:04:47 +0300

    done hm_6

--- Lesson6/hm_6/catalog.js
+++ Lesson6/hm_6/catalog.js
@@ -45,21 +45,21 @@ const catalog = {
 
 
     createHtml() {
-        let i = 0
+        let i = 0;
         this.goods.forEach(good => {
-            const box = document.createElement('div')
-            box.classList.add('good')
+            const box = document.createElement('div');
+            box.classList.add('good');
 
-            box.insertAdjacentHTML("beforeend", this.render(good))
+            box.insertAdjacentHTML("beforeend", this.render(good));
 
-            const good_btn = document.createElement('a')
-            good_btn.innerText = 'Добавить в козину'
-            good_btn.classList.add('button')
-            good_btn.setAttribute('href', '#')
-            good_btn.dataset.id = `${i++}`
-            box.appendChild(good_btn)
+            const good_btn = document.createElement('a');
+            good_btn.innerText = 'Добавить в козину';
+            good_btn.classList.add('button');
+            good_btn.setAttribute('href', '#');
+            good_btn.dataset.id = `${i++}`;
+            box.appendChild(good_btn);
 
-            this.containerElement.appendChild(box)
+            this.containerElement.appendChild(box);
 
 
         })
@@ -69,10 +69,12 @@ const catalog = {
 
 catalog.createHtml();
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////   Cart  ///////////////////////////////////////////////////////////////////
 
 const goodsCart = {
 
+    goods: [],
+    getCount: {},
     cartContainer: document.getElementById('cart'),
 
     render(good) {
@@ -83,60 +85,64 @@ const goodsCart = {
 `
     },
 
-    goods: [],
-
     countBasketPrice() {
         return this.goods.reduce((totalCost, costItem) =>
-                totalCost += costItem.cost * 1
+                totalCost += costItem.cost * costItem.count
+            , 0)
+    },
+
+    totalCount() {
+        return this.goods.reduce((totalCost, costItem) =>
+                totalCost += costItem.count
             , 0)
     },
 
     getTotalCostCart() {
 
-        this.cartContainer.innerText = ''
+        this.cartContainer.innerText = '';
         if (this.countBasketPrice() === 0) {
-            this.cartContainer.insertAdjacentHTML('afterbegin', ` < h4 > Козина
-            пуста < /h4>`);
-        } else this.createGood();
+            this.cartContainer.insertAdjacentHTML('afterbegin', '<h4> Козина пуста </h4>');
+        } else {
+            this.createGood();
 
-        const cartInfo = document.createElement('h4');
-        cartInfo.innerText = ``
-        cartInfo.innerText = `В корзине: ${this.goods.length} товарa(ов) на сумму ${this.countBasketPrice()} рублей`
-        this.cartContainer.appendChild(cartInfo)
+            const cartInfo = document.createElement('h4');
+            cartInfo.innerText = ``;
+            cartInfo.innerText = `В корзине: ${this.totalCount()} товарa(ов) на сумму ${this.countBasketPrice()} рублей`;
+            this.cartContainer.appendChild(cartInfo);
+
+            this.cartContainer.insertAdjacentHTML('beforeend', '<a id = "buy" href = "#"> Купить</a>');
+            const buyBtn = document.getElementById('buy');
+            buyBtn.addEventListener("click", () => this.clearGoods());
+        }
     },
 
     createGood() {
 
         this.goods.forEach(good => {
-            const box = document.createElement('div')
-            box.classList.add('good')
+            const box = document.createElement('div');
+            box.classList.add('good');
 
-            box.insertAdjacentHTML("beforeend", this.render(good))
+            box.insertAdjacentHTML("beforeend", this.render(good));
 
-            this.cartContainer.appendChild(box)
-
+            this.cartContainer.appendChild(box);
 
         })
+
+
     },
 
-}
+    clearGoods() {
+        this.goods = []
+        this.getCount = {}
+        this.getTotalCostCart()
+    },
+
+};
 
 
 const addGoods = {
     catalog,
     goodsCart,
-    // buttonNum: null,
-
-    addGood(id) {
-        // this.buttonNum = id;
-
-        this.goodsCart.goods.push(this.catalog.goods[id])
-        this.goodsCart.goods[id].count = 9
-        console.log(this.goodsCart.goods)
-        console.log(goodsCart.countBasketPrice())
-        this.goodsCart.getTotalCostCart()
-    },
-
 
     clickButtonAddGood() {
 
@@ -149,11 +155,20 @@ const addGoods = {
     containerClickHandler(event) {
 
         if (event.target.tagName !== 'A') return;
+
+        this.goodsCart.getCount[event.target.dataset.id] = this.goodsCart.getCount[event.target.dataset.id] === undefined ? 1 : this.goodsCart.getCount[event.target.dataset.id] + 1;
         this.addGood(event.target.dataset.id);
 
     },
 
-}
+    addGood(id) {
 
+        this.goodsCart.goods[id] = (this.catalog.goods[id]);
+        this.goodsCart.goods[id]['count'] = this.goodsCart.getCount[id];
+        this.goodsCart.getTotalCostCart();
+    },
+
+};
+
+goodsCart.getTotalCostCart();
 addGoods.clickButtonAddGood();
-console.log(goodsCart)
